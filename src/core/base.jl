534b8cf92e0dff521ba1a95608e35fb59db9859b
alejandroowen <69917992+alejandroowen@users.noreply.github.com>
2020-08-20 15:17:25 -0400

    Update base.jl

--- src/core/base.jl
+++ src/core/base.jl
@@ -1,4 +1,4 @@
-# stuff that is universal to all power models
+# stuff that is universal to all power modelss
 
 "root of the power formulation type hierarchy"
 abstract type AbstractPowerModel <: _IM.AbstractInfrastructureModel end
